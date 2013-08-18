aa7e836ddb281a03953fc4f7c13cbe86198fa045
Sam Coles <sammuel.coles@gmail.com>
2013-08-18 18:17:08 -0500

    LET THERE BE PANNING, and it was good

--- Sound/Mixer/Noize.hs
+++ Sound/Mixer/Noize.hs
@@ -3,25 +3,53 @@
 module Main where
 
 import Foreign.Ptr
+import qualified Data.Map.Strict as Map
+
+--JUNK FOR TESTING ONLY--
 import Control.Concurrent (threadDelay)
 import Control.Monad
 
 --import Sound.SFML
 import Sound.SFML.LowLevel
 
-data Mixer = Mixer {    channels    :: Maybe [Channel],
+data Mixer = Mixer {    channels    :: Maybe (Map.Map String Channel),
                         masterVol   :: Float,
                         music       :: Maybe (Ptr Music)
                    }
+    deriving (Show)
 
 data Channel = Channel {    sndData :: Ptr Sound,
+                            alias   :: String,
                             status  :: Status,
                             volume  :: Float
                        }
-
+    deriving (Show)
 
 initMixer :: Mixer
 initMixer = Mixer Nothing 100.0 Nothing
+
+addChannel :: Mixer -> FilePath -> String -> Float -> IO (Mixer)
+addChannel mix@(Mixer chans vol music) inFile name chanVol = do
+    case music of
+        Nothing -> do
+            chan <- allocateChan
+            let chans' = Just (Map.singleton name chan)
+            return (Mixer chans' vol music)
+
+        Just m -> do
+            chan <- allocateChan
+            let chans' = liftM (Map.insert name chan) chans
+            return (Mixer chans' vol music)
+
+    where allocateChan = do
+            buffer  <- sfSoundBuffer_CreateFromFile inFile
+            snd     <- sfSound_Create
+            sfSound_SetBuffer snd buffer
+
+            sfSound_SetVolume snd chanVol
+            stat <- getSoundStatus snd
+
+            return (Channel snd name stat chanVol)
 
 loadMusic :: Mixer -> FilePath -> IO (Mixer)
 loadMusic (Mixer chans vol music) inFile = do
@@ -50,10 +78,33 @@ playMusic mix@(Mixer chans vol music) = do
         Just music -> do
             sfMusic_Play music
             return mix
-            
+          
+setPan :: Mixer -> String -> (Float, Float, Float) -> IO (Mixer) 
+setPan mix@(Mixer chans vol music) chanName (x, y, z) = do
+    case chans of
+        Nothing -> do
+            print ("NO CHANNELS TO PAN")
+            return mix
+        Just chans -> do
+            let chan = chans Map.! chanName
+            sfSound_SetPosition (sndData chan) x y z
+            return mix
+
+startChannel mix@(Mixer chans vol music) chanName = do
+    case chans of
+        Nothing -> do
+            print ("NO CHANNELS TO START")
+            return mix
+        Just chans -> do
+            let chan = chans Map.! chanName
+            sfSound_Play (sndData chan)
+            return mix
 main = do
     let mix = initMixer
-        inFile = "/home/sam/src/Noize/shpongle.wav"
-    mix' <- loadMusic mix inFile
-    playMusic mix'
+        inFile = "/home/sam/src/Noize/shpongle-mono.ogg"
+    --mix' <- loadMusic mix inFile
+    --playMusic mix'
+    mix' <- addChannel mix inFile "shpongle" 100.0
+    startChannel mix' "shpongle"
+    setPan mix' "shpongle" (0.0, 100.0, 0.0)
     forever $ do (threadDelay 10000)
